39a4e6687a462dfeb8ca2f2d8529d1278a35a94c
Ruy-u <119630026+Ruy-u@users.noreply.github.com>
2023-09-10 03:48:50 +0300

    Add files via upload

---
+++ Predator Animals Patch/1.4/Source/PAP/PredatorAnimalsPatch/PredatorAnimalsPatch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Ruyu.PredatorAnimalsPatch
+{
+
+    [HarmonyPatch(typeof(JobDriver_PredatorHunt))]
+    [HarmonyPatch("MakeNewToils")]
+    public static class PatchHitAction
+    {
+        private static void Postfix(JobDriver_PredatorHunt __instance)
+        {
+            // Check if the predator successfully hunted its prey
+            if (__instance.firstHit)
+            {
+                Pawn predator = __instance.pawn;
+                HediffDef reducedBleedingHediffDef = DefDatabase<HediffDef>.GetNamed("ruyuHediff_ReducedBleeding"); // Replace with your hediff definition name
+                Hediff reducedBleedingHediff = HediffMaker.MakeHediff(reducedBleedingHediffDef, predator);
+                predator.health.AddHediff(reducedBleedingHediff);
+            }
+        }
+
+        [StaticConstructorOnStartup]
+        public static class PredatorAnimalsPatch
+        {
+            static PredatorAnimalsPatch()
+            {
+                Harmony harmony = new Harmony("Ruyu.PredatorAnimalsPatch");
+                harmony.PatchAll();
+            }
+        }
+    }
+}
